d612438943d1d2e94a49c93bd69e2744156b3000
MaybeAPotato <32529003+MaybeAPotato@users.noreply.github.com>
2017-10-06 15:14:49 -0400

    Basic idea of the assignment

---
+++ ChessAssignment.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chessassignment;
+import chessStuff.chessInterface;
+import chessStuff.chessPiece;
+
+/**
+ *
+ * @author User
+ */
+public class ChessAssignment {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Chess boards are 8 by 8                        [y][x]
+        chessInterface[][] gameBoard = new chessInterface[8][8];
+    }
+    
+}
