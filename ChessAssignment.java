29f4c41ad411de5845239130b508e815ff2d44f3
JudenConnor <32580536+JudenConnor@users.noreply.github.com>
2017-10-07 12:36:54 -0400

    Add files via upload
    In Ottawa for thanksgiving.  I'll be working sparsely while family events are happening.
    -Connor Mac

--- ChessAssignment.java
+++ ChessAssignment.java
@@ -1,26 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- * hacked expertly, by Connor
- */
 package chessassignment;
 import chessStuff.chessInterface;
-import chessStuff.chessPiece;
+import chessStuff.pieceTemplate;
 
-/**
- *
- * @author User
- */
+//Connor's edition
+
 public class ChessAssignment {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
-        //Chess boards are 8 by 8                        [y][x]
-        chessInterface[][] gameBoard = new chessInterface[8][8];
+        chessInterface[][] gameBoard = new chessInterface[8][8]; //Board is 8 by 8
     }
     
+    public void printBoard(){
+        
+    }
 }
--- chessInterface.java
+++ chessInterface.java
@@ -1,26 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package chessStuff;
 
-/**
- *
- * @author User
- */
+//Connor's edition
+
 public interface chessInterface {
-    //Checks if the piece can move to the input position
-    //Position must be within board
-    //Position must be free from friendly pieces
-    //Available postions will be based off role
-    //Pawn - one infront or diagonal if enemy piece is present
-    //Bishop - diagonal
-    //Rook - horizontal and vertical
-    //Knight(C) - two squares horizontal or vertical then one square perpindicular
-    //Queen - horizontal, vertical, and diagonal
-    //King - one sqaure horizontal or vertical (will not check for check or checkmate, game will end once a players King is dead)
-    boolean canMoveTo(int x, int y);
-    //Will acutally move the piece
-    void moveTo(int x, int y);
+   
+    void setPlayer(int _nPlayer); //Sets the piece's controlling player
+    void movePiece(int _nX, int _nY); //Attempts to move piece
+    
+    boolean checkMove(int _nX, int _nY); //Checks to see if move is possible
+    boolean onBoard(int _nX, int _nY); //Checks to see if coordinates are on the board
+    
+    String getName(); //Get piece name
 }
